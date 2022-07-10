86c0745c228ef5262532e006b298c10b5d145a0e
Yasmine <Yasmine@DESKTOP-QF8P549>
2022-07-10 23:25:22 +0200

    Ajoutez des fichiers projet.

---
+++ API/IServices/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+namespace API.IServices
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetEmployee();
+        Employee GetEmployeeById(int id);
+        Employee AddEmployee(Employee employee);
+        Employee UpdateEmployee(Employee employee);
+        Employee DeleteEmployee(int id);
+    }
+}
---
+++ API/Models/APICoreDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace API.Models
+{
+    public partial class APICoreDBContext : DbContext
+    {
+        public APICoreDBContext()
+        {
+        }
+
+        public APICoreDBContext(DbContextOptions<APICoreDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                //optionsBuilder.UseSqlServer("Server=.; Database=APICoreDB;Trusted_Connection=True; ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Department)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(10, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
